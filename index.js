290b28fd16594c0ae470565c9e59a30f15fe3503
sagar gohel <sagar.180180107011@gmail.com>
2022-12-06 16:10:51 +0530

    api for add task and get list of task

--- index.js
+++ index.js
@@ -16,11 +16,39 @@ const todos = [
   },
 ];
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({
-    success: true,
-    data: todos,
-  }));
+  const { method, url } = req;
+  let body = [];
+  req
+    .on("data", (data) => {
+      body.push(data);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      let status = 404;
+      const response = {
+        success: true,
+        data: null,
+      };
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === "POST" && url === "/todos") {
+        const { id, task } = JSON.parse(body);
+        if (!id || !task) {
+          status = 400;
+          response.success = false;
+          response.data = null;
+       
+        } else {
+           todos.push({ id, task });
+        status = 201;
+        response.success = true;
+        response.data = todos;
+      }
+      res.writeHead(status, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(response));
+    });
 });
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Sever is running on port :", PORT));
