8b83ed0e470a2d0c77843165bbcbbec119234fa6
sagar gohel <sagar.180180107011@gmail.com>
2022-12-06 14:08:21 +0530

    index js changed

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const path = require("path");
 const fs = require("fs");
 const http = require("http");
+const todos = [
+  {
+    id: 1,
+    task: "task 1",
+  },
+  {
+    id: 2,
+    task: "task 2",
+  },
+  {
+    id: 3,
+    task: "task 3",
+  },
+];
 const server = http.createServer((req, res) => {
-  res.setHeader("content-type", "text/html");
-  res.write("<h1>hello world!</h1>");
-  res.end("backend developers!");
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({
+    success: true,
+    data: todos,
+  }));
 });
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Sever is running on port :", PORT));
